3aa75fa118562be9c891e9003a939e01cd1d6b30
QuangMinh02052004 <lequangminh951@gmail.com>
2026-03-22 20:58:41 +0700

    them vao file CircleCiApplication dong print

--- src/main/java/nhom5/circleci/CircleCiApplication.java
+++ src/main/java/nhom5/circleci/CircleCiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CircleCiApplication {
 
     public static void main(String[] args) {
+        System.out.println("CircleCI");
         SpringApplication.run(CircleCiApplication.class, args);
     }
 
